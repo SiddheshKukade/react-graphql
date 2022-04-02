004f2634c48b794ebf4a28f8343f5c2323d3990a
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-04-02 16:07:53 +0530

    Added dummy data functon

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const express = require("express");
 // the express graph ql allows express to understand graphql
 const graphHTTP = require("express-graphql");
-
+const schema = require("./schema/schema");
 const app = express();
 
-app.use("/graphql", graphHTTP({}));
+app.use("/graphql", graphHTTP({ schema }));
 
 app.listen(4000, () => {
   console.log("yo lo ");

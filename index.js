e4d158ff1ee039e8939a9f4bd57258817f9e0f6d
Siddhesh Bhupendra Kuakde <65951872+SiddheshKukade@users.noreply.github.com>
2022-04-02 13:01:59 +0530

    Basic Graphql Setup Completed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Creating a Graph-QL Server
 const express = require("express");
+// the express graph ql allows express to understand graphql
+const graphHTTP = require("express-graphql");
+
 const app = express();
+
+app.use("/graphql", graphHTTP({}));
+
 app.listen(4000, () => {
   console.log("yo lo ");
 });
